cfee20e5955d61e44585bc24f317e43059d2310f
Christian Mönch <christian.moench@web.de>
2021-03-10 11:37:53 +0100

    RF: use fasteners intra-process lock

--- dataladmetadatamodel/mapper/gitmapper/utils.py
+++ dataladmetadatamodel/mapper/gitmapper/utils.py
@@ -5,56 +5,68 @@ are automatically released when a process exits. So no
 stale locks are kept around, even if you kill a runaway-
 process.
 """
-import fcntl
 import logging
 import os
 import time
-from typing import IO, Optional, Tuple
 
 from dataclasses import dataclass
+from fasteners import InterProcessLock
 
-
-PID = os.getpid()
-GIT_MAPPER_LOCK_FILE_NAME = ".datalad-git-backend.lock"
 
 logger = logging.getLogger("datalad.metadata.model")
+PID = os.getpid()
 
+GIT_MAPPER_LOCK_FILE_NAME = ".datalad-git-backend.lock"
 read_write_locked = dict()
+sleep_time = .1
 
 
 @dataclass
 class LockState:
     counter: int
-    file_object: Optional[IO]
+    lock: InterProcessLock
 
 
-def _get_locked_file(realm: str, mode: int) -> Tuple[IO, float]:
-    lock_file = open(realm + "/" + GIT_MAPPER_LOCK_FILE_NAME, "w")
+def _get_lock_file(realm: str) -> float:
+
     lock_time = time.time()
-    fcntl.lockf(lock_file, mode)
+    while True:
+        try:
+            lock_file = open(os.path.join(realm, GIT_MAPPER_LOCK_FILE_NAME, "wx"))
+            break
+        except FileExistsError:
+            time.sleep(.1)
     lock_time = time.time() - lock_time
+
     lock_file.write(str(PID) + "\n")
     lock_file.flush()
-    return lock_file, lock_time
-
-
-def _unlock_file(lock_file: IO):
-    lock_file.truncate(0)
-    fcntl.lockf(lock_file, fcntl.LOCK_UN)
     lock_file.close()
+    return lock_time
+
+
+def _remove_lock_file(realm: str):
+    os.unlink(os.path.join(realm, GIT_MAPPER_LOCK_FILE_NAME))
 
 
 def _get_lock_state(lock_dict: dict, realm: str) -> LockState:
     if realm not in lock_dict:
-        lock_dict[realm] = LockState(0, None)
+        lock_dict[realm] = LockState(
+            0,
+            InterProcessLock(os.path.join(realm, GIT_MAPPER_LOCK_FILE_NAME)))
     return lock_dict[realm]
 
 
 def lock_backend(realm: str):
     lock_state = _get_lock_state(read_write_locked, realm)
     if lock_state.counter == 0:
-        lock_state.file_object, lock_time = _get_locked_file(realm, fcntl.LOCK_EX)
-        logger.debug("process {} read-write-locked git backend {} in {} seconds".format(PID, realm, lock_time))
+        lock_time = time.time()
+        lock_state.lock.acquire()
+        lock_time = time.time() - lock_time
+        logger.debug(
+            "process {} locked git backend {} in {} seconds".format(
+                PID,
+                realm,
+                int(lock_time)))
     lock_state.counter += 1
 
 
@@ -63,6 +75,5 @@ def unlock_backend(realm: str):
     assert lock_state.counter > 0
     lock_state.counter -= 1
     if lock_state.counter == 0:
-        logger.debug("process {} read-write-unlocks git backend {}".format(PID, realm))
-        _unlock_file(lock_state.file_object)
-        lock_state.file_object = None
+        logger.debug("process {} unlocks git backend {}".format(PID, realm))
+        lock_state.lock.release()
