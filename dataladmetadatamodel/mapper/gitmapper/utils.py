5888041f5d7993f9f26dbfdd0de4e369cc6478fe
Christian Mönch <christian.moench@web.de>
2021-04-23 17:06:04 +0200

    RF: use Path in backend-locking

--- dataladmetadatamodel/mapper/gitmapper/utils.py
+++ dataladmetadatamodel/mapper/gitmapper/utils.py
@@ -8,17 +8,17 @@ process.
 import logging
 import os
 import time
-
 from dataclasses import dataclass
+from pathlib import Path
+
 from fasteners import InterProcessLock
 
 
 logger = logging.getLogger("datalad.metadata.model")
 PID = os.getpid()
 
-GIT_MAPPER_LOCK_FILE_NAME = ".datalad-git-backend.lock"
+GIT_MAPPER_LOCK_FILE_NAME = "metadata-model-git.lock"
 read_write_locked = dict()
-sleep_time = .1
 
 
 @dataclass
@@ -27,15 +27,15 @@ class LockState:
     lock: InterProcessLock
 
 
-def _get_lock_state(lock_dict: dict, realm: str) -> LockState:
+def _get_lock_state(lock_dict: dict, realm: Path) -> LockState:
     if realm not in lock_dict:
         lock_dict[realm] = LockState(
             0,
-            InterProcessLock(os.path.join(realm, GIT_MAPPER_LOCK_FILE_NAME)))
+            InterProcessLock(str(realm / GIT_MAPPER_LOCK_FILE_NAME)))
     return lock_dict[realm]
 
 
-def lock_backend(realm: str):
+def lock_backend(realm: Path):
     lock_state = _get_lock_state(read_write_locked, realm)
     if lock_state.counter == 0:
         lock_time = time.time()
@@ -49,7 +49,7 @@ def lock_backend(realm: str):
     lock_state.counter += 1
 
 
-def unlock_backend(realm: str):
+def unlock_backend(realm: Path):
     lock_state = _get_lock_state(read_write_locked, realm)
     assert lock_state.counter > 0
     lock_state.counter -= 1
